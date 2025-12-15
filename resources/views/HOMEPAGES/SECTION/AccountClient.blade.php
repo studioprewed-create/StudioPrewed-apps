61bde7bf0dfbf54a653ffa19e75a471450568444
Studioprewed <studioprewed@gmail.com>
2025-12-15 15:43:29 +0700

    format Riwayat client

--- resources/views/HOMEPAGES/SECTION/AccountClient.blade.php
+++ resources/views/HOMEPAGES/SECTION/AccountClient.blade.php
@@ -99,12 +99,11 @@
                     </div>
                 </div>
 
-                <div class="info-group">
-                    <label>Tanggal Lahir Pasangan</label>
-                    <div class="info-value">
-                        {{ optional($dataDiri)->tanggal_lahir_pasangan ?? '-' }}
+                <div class="info-value">
+                    {{ optional($dataDiri)->tanggal_lahir_pasangan
+                        ? \Carbon\Carbon::parse($dataDiri->tanggal_lahir_pasangan)->format('d F Y')
+                        : '-' }}
                     </div>
-                </div>
             </div>
             <div class="profile-actions">
                 <button type="button" class="akun-edit-btn"
