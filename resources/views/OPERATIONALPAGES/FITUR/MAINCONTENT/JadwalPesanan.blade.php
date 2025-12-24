d918f8d8cae26c3b4cdf19122d18979ca1c6bda7
Studioprewed <studioprewed@gmail.com>
2025-12-24 19:05:16 +0700

    jadwal pesanan

--- resources/views/OPERATIONALPAGES/FITUR/MAINCONTENT/JadwalPesanan.blade.php
+++ resources/views/OPERATIONALPAGES/FITUR/MAINCONTENT/JadwalPesanan.blade.php
@@ -92,35 +92,90 @@
         <div class="booking-modal" id="bookingModal-{{ $booking->id }}">
             <div class="booking-modal-backdrop"></div>
 
-            <div class="booking-modal-content">
-                <button class="booking-modal-close">&times;</button>
+            <div class="booking-modal-content modal-xl">
 
-                <h3>Detail Booking</h3>
+                <div class="modal-header">
+                    <h5>Detail Booking</h5>
+                    <button class="booking-modal-close">&times;</button>
+                </div>
 
-                <div class="modal-grid">
-                    <div><strong>Kode</strong><br>#{{ $booking->kode_pesanan }}</div>
-                    <div><strong>Status</strong><br>{{ ucfirst($booking->status) }}</div>
+                <div class="modal-body">
+                    <div class="form-grid-2">
 
-                    <div><strong>Paket</strong><br>{{ $booking->package->nama_paket ?? '-' }}</div>
-                    <div><strong>Style</strong><br>{{ $booking->style ?? '-' }}</div>
+                        <div class="form-group">
+                            <strong>Kode Pesanan</strong>
+                            <div>#{{ $booking->kode_pesanan }}</div>
+                        </div>
 
-                    <div><strong>Tanggal</strong><br>{{ $booking->photoshoot_date->format('d F Y') }}</div>
-                    <div><strong>Slot</strong><br>{{ $booking->photoshoot_slot }}</div>
+                        <div class="form-group">
+                            <strong>Status</strong>
+                            <div>{{ ucfirst($booking->status) }}</div>
+                        </div>
 
-                    <div><strong>CPP</strong><br>{{ $booking->nama_cpp }}</div>
-                    <div><strong>CPW</strong><br>{{ $booking->nama_cpw }}</div>
+                        <div class="form-group">
+                            <strong>Paket</strong>
+                            <div>{{ $booking->package->nama_paket ?? '-' }}</div>
+                        </div>
 
-                    <div><strong>Tema Utama</strong><br>{{ $booking->tema_nama }}</div>
-                    <div><strong>Tema Tambahan</strong><br>{{ $booking->tema2_nama ?? '-' }}</div>
+                        <div class="form-group">
+                            <strong>Style</strong>
+                            <div>{{ $booking->style ?? '-' }}</div>
+                        </div>
 
-                    <div><strong>Addon</strong><br>{{ $booking->addons_total_formatted }}</div>
-                    <div><strong>Total</strong><br>{{ $booking->grand_total_formatted }}</div>
+                        <div class="form-group">
+                            <strong>Tanggal</strong>
+                            <div>{{ $booking->photoshoot_date->format('d F Y') }}</div>
+                        </div>
 
-                    <div style="grid-column:1/-1">
-                        <strong>Catatan</strong><br>
-                        {{ $booking->notes ?? '-' }}
+                        <div class="form-group">
+                            <strong>Slot</strong>
+                            <div>{{ $booking->photoshoot_slot }}</div>
+                        </div>
+
+                        <div class="form-group">
+                            <strong>CPP</strong>
+                            <div>{{ $booking->nama_cpp }}</div>
+                        </div>
+
+                        <div class="form-group">
+                            <strong>CPW</strong>
+                            <div>{{ $booking->nama_cpw }}</div>
+                        </div>
+
+                        <div class="form-group">
+                            <strong>Tema Utama</strong>
+                            <div>{{ $booking->tema_nama }}</div>
+                        </div>
+
+                        <div class="form-group">
+                            <strong>Tema Tambahan</strong>
+                            <div>{{ $booking->tema2_nama ?? '-' }}</div>
+                        </div>
+
+                        <div class="form-group">
+                            <strong>Addon</strong>
+                            <div>{{ $booking->addons_total_formatted }}</div>
+                        </div>
+
+                        <div class="form-group">
+                            <strong>Total</strong>
+                            <div>{{ $booking->grand_total_formatted }}</div>
+                        </div>
+
+                        <div class="form-group" style="grid-column:1/-1">
+                            <strong>Catatan</strong>
+                            <div>{{ $booking->notes ?? '-' }}</div>
+                        </div>
+
                     </div>
                 </div>
+
+                <div class="modal-footer">
+                    <button class="jp-btn jp-btn-ghost booking-modal-close">
+                        Tutup
+                    </button>
+                </div>
+
             </div>
         </div>
     @endforeach
