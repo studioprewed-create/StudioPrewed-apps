09a0bdd4f760802a10d902eee22f63bd37ca1e75
Studioprewed <studioprewed@gmail.com>
2026-01-05 14:43:17 +0700

    jadwal pesanan filter

--- resources/views/HOMEPAGES/MODAL/RiwayatClient.blade.php
+++ resources/views/HOMEPAGES/MODAL/RiwayatClient.blade.php
@@ -7,6 +7,8 @@
         <h3>Detail Booking</h3>
 
         <div class="modal-grid">
+            <h3 class="modal-section-title">Data Paket</h3>
+
             <div><strong>Kode Pesanan</strong><br>#{{ $booking->kode_pesanan }}</div>
             <div><strong>Status</strong><br>{{ ucfirst($booking->status) }}</div>
 
@@ -16,18 +18,31 @@
             <div><strong>Tanggal</strong><br>{{ $booking->photoshoot_date->format('d F Y') }}</div>
             <div><strong>Slot</strong><br>{{ $booking->photoshoot_slot }}</div>
 
-            <div><strong>CPP</strong><br>{{ $booking->nama_cpp }}</div>
-            <div><strong>CPW</strong><br>{{ $booking->nama_cpw }}</div>
+            <div><strong>Tanggal</strong><br>{{ $booking->photoshoot_date->format('d F Y') }}</div>
+            <div><strong>Extra Slot</strong><br>{{ $booking->extra_photoshoot_slot }}</div>
 
             <div><strong>Tema Utama</strong><br>{{ $booking->tema_nama }} ({{ $booking->tema_kode }})</div>
             <div><strong>Tema Tambahan</strong><br>{{ $booking->tema2_nama ?? '-' }}</div>
 
+            <h3 class="modal-section-title">Data costumer</h3>
+
+            <div><strong>Nama CPP</strong><br>{{ $booking->nama_cpp }}</div>
+            <div><strong>Nama CPW</strong><br>{{ $booking->nama_cpw }}</div>
+
+            <div><strong>Email CPP</strong><br>{{ $booking->email_cpp }}</div>
+            <div><strong>Email CPW</strong><br>{{ $booking->email_cpw }}</div>
+
+            <div><strong>No CPP</strong><br>{{ $booking->phone_cpp }}</div>
+            <div><strong>No CPW</strong><br>{{ $booking->phone_cpw }}</div>
+
+            <h3 class="modal-section-title">Harga & Tambahan</h3>
+            
+            <div><strong>Harga</strong><br>{{ $booking->package_price_formatted }}</div>
             <div><strong>Addon</strong><br>{{ $booking->addons_total_formatted }}</div>
-            <div><strong>Total</strong><br>{{ $booking->grand_total_formatted }}</div>
 
             <div style="grid-column:1/-1">
-                <strong>Catatan</strong><br>
-                {{ $booking->notes ?? '-' }}
+                <strong>Total</strong><br>
+                {{ $booking->grand_total_formatted}}
             </div>
         </div>
     </div>
