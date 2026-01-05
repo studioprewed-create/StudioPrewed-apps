0ca530dbc381f5e7398874d1b944a61f3559f90a
Studioprewed <studioprewed@gmail.com>
2026-01-05 15:31:26 +0700

    jadwal pesanan

--- resources/views/HOMEPAGES/MODAL/RiwayatClient.blade.php
+++ resources/views/HOMEPAGES/MODAL/RiwayatClient.blade.php
@@ -41,7 +41,7 @@
         <h3 class="modal-section-title">Harga & Tambahan</h3>
 
         <div class="modal-grid">   
-            <div><strong>Harga</strong><br>{{ $booking->package_price_formatted }}</div>
+            <div><strong>Harga</strong><br>{{ $booking->package_price }}</div>
             <div><strong>Addon</strong><br>{{ $booking->addons_total_formatted }}</div>
 
             <div style="grid-column:1/-1">
