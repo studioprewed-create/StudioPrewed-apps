40667187251770ccdc8d30a9da405a000fbabaed
Studioprewed <studioprewed@gmail.com>
2026-02-13 13:42:57 +0700

    jadwal pesanan filter

--- resources/views/HOMEPAGES/PAGE/Portofolio.blade.php
+++ resources/views/HOMEPAGES/PAGE/Portofolio.blade.php
@@ -9,7 +9,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
     <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
-   <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
   <style>
   </style>
