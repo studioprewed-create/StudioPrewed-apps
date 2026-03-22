42846ddb79137600bf30f0768cefc46ebe5f4ee7
Studioprewed <studioprewed@gmail.com>
2026-03-23 02:39:25 +0700

    Refactor css

--- resources/views/HOMEPAGES/PAGE/Dashboard.blade.php
+++ resources/views/HOMEPAGES/PAGE/Dashboard.blade.php
@@ -29,7 +29,6 @@
         @include('HOMEPAGES.SECTION.Marquee')
         @include('HOMEPAGES.SECTION.Aboutus')
         @include('HOMEPAGES.SECTION.WhyUs')
-        @include('HOMEPAGES.SECTION.Review')
         @include('HOMEPAGES.SECTION.FAQ')
         @include('HOMEPAGES.SECTION.Footer')
     </main>
