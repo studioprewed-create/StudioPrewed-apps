523f7b7f978091578e3c93128206af7368659609
Studioprewed <studioprewed@gmail.com>
2026-04-01 16:27:42 +0700

    Refactor css

--- resources/views/HOMEPAGES/MODAL/PackageModal.blade.php
+++ resources/views/HOMEPAGES/MODAL/PackageModal.blade.php
@@ -87,6 +87,21 @@
             <div class="pkg-item">
               <i class="fas fa-sticky-note"></i>
               <div>
+                <h5>Syarat & Ketentuan</h5>
+                <ul class="pkg-notes">
+                  <li>Harap melakukan pembayaran DP sebesar Rp.250.000</li>
+                  <li>Khusus untuk weekend dan tanggal merah DP sebesar Rp.500.000</li>
+                  <li>Khusus untuk Adat Sumatera, DP minimal sejumlah harga charge attire</li>
+                  <li>Pelunasan maksimal H-1 sebelum pemotretan berlangsung</li>
+                  <li>Jika ada pembatalan booking/cancel dianggap hangus</li>
+                  <li>Free reschedule 1x, infokan maksimal H-3 dari jadwal awal</li>
+                  <li>Datang 2 jam sebelum booking (2,5 jam untuk adat Sumatera)</li>
+                </ul>
+              </div>
+            </div>
+            <div class="pkg-item">
+              <i class="fas fa-sticky-note"></i>
+              <div>
                 <h5>Notes</h5>
                 <p>{{ $pkg->notes ?: '-' }}</p>
               </div>
