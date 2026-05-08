172f55317c053e97dd1e5eb26e43f60fc880ddf6
Studioprewed <studioprewed@gmail.com>
2026-05-08 17:19:01 +0700

    survey

--- resources/views/HOMEPAGES/SECTION/Survey.blade.php
+++ resources/views/HOMEPAGES/SECTION/Survey.blade.php
@@ -1,31 +1,25 @@
 <section class="survey-cta-section">
+    <div class="survey-cta-card reveal-init">
 
-    <div class="container">
+        <span class="eyebrow">
+            CUSTOMER FEEDBACK
+        </span>
 
-        <div class="survey-cta-card reveal-init">
+        <h2>
+            Bantu Kami Menjadi <span class="hl">Lebih Baik</span>
+        </h2>
 
-            <span class="eyebrow">
-                CUSTOMER FEEDBACK
-            </span>
+        <p>
+            Masukan dari kamu sangat berarti untuk meningkatkan kualitas pelayanan Studio Prewed ✨
+        </p>
 
-            <h2>
-                Bantu Kami Menjadi <span class="hl">Lebih Baik</span>
-            </h2>
+        <div class="survey-cta-action">
 
-            <p>
-                Masukan dari kamu sangat berarti untuk meningkatkan kualitas pelayanan Studio Prewed ✨
-            </p>
-
-            <div class="survey-cta-action">
-
-                <a href="{{ route('Survey') }}" class="survey-cta-btn">
-                    Isi Survey
-                </a>
-
-            </div>
+            <a href="{{ route('Survey') }}" class="survey-cta-btn">
+                Isi Survey
+            </a>
 
         </div>
 
     </div>
-
 </section>
