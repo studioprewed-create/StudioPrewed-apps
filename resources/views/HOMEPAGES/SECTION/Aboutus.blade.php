7a8c2b0755aab449e29a9873f7fb314165378899
Studioprewed <studioprewed@gmail.com>
2026-05-17 01:24:15 +0700

    about

--- resources/views/HOMEPAGES/SECTION/Aboutus.blade.php
+++ resources/views/HOMEPAGES/SECTION/Aboutus.blade.php
@@ -96,4 +96,41 @@
             </div>
         </div>
     </div>
+
+    <section class="survey-cta">
+
+        <div class="container">
+
+            <div class="survey-cta-box">
+
+                <div class="survey-cta-content">
+
+                    <span class="eyebrow">
+                        CUSTOMER FEEDBACK
+                    </span>
+
+                    <h2>
+                        Bantu Kami Menjadi
+                        <span class="hl">Lebih Baik</span>
+                    </h2>
+
+                    <p>
+                        Masukan dari kamu sangat berarti untuk meningkatkan kualitas pelayanan Studio Prewed.
+                    </p>
+
+                </div>
+
+                <div class="survey-cta-action">
+
+                    <a href="{{ route('Survey') }}" class="survey-cta-btn">
+                        Isi Survey
+                    </a>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </section>
 </section>
