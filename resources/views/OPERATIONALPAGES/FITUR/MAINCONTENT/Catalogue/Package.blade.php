3213c5668fdc1b13551a17ec33970045316f585b
Studioprewed <studioprewed@gmail.com>
2026-06-10 22:47:25 +0700

    ubah struktur

--- resources/views/OPERATIONALPAGES/FITUR/MAINCONTENT/Catalogue/Package.blade.php
+++ resources/views/OPERATIONALPAGES/FITUR/MAINCONTENT/Catalogue/Package.blade.php
@@ -48,10 +48,10 @@
             Belum ada package. Klik <b>Tambah Package</b> untuk menambahkan.
         </div>
     @else
-        <div class="grid-cards-package">
+        <div class="grid-cards">
             @foreach ($packages as $p)
-                <article class="card-elev-package">
-                    <div class="card-grid-package">
+                <article class="card-elev">
+                    <div class="card-grid">
                         <div class="card-media">
                             <div class="ratio-3x4">
                                 <img src="{{ $p->image_url }}" alt="{{ $p->nama_paket }}">
